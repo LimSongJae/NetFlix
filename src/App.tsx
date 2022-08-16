b7d9a2ac7589ff8ad3118df72a278cffb54989a5
LimSongJae <thdwo999@naver.com>
2022-08-16 23:48:09 +0900

    feat. Header Component 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import TV from "./Routes/TV";
+
 function App() {
   return (
-    <div>
-      jyu
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<TV />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
